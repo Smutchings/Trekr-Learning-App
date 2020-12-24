80266d0ed7aca4335f22396890060077c201fc80
Sam Hutchings <sam.hutchings@icloud.com>
2020-12-24 14:28:22 +0000

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Sam Hutchings on 24/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
